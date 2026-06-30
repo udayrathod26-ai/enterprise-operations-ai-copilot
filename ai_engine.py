71f7aedf8d38f8c258cff880143dce40495d52e5
Uday Rathod <udaysinghrathod2024@gmail.com>
2026-06-30 15:17:15 +0530

    Updated Gemini model

--- ai_engine.py
+++ ai_engine.py
@@ -16,7 +16,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # -----------------------------------
 # Business Question Summary
