efb02c735b818eb63c98ab877ff052d6c9bc59ae
Uday Rathod <udaysinghrathod2024@gmail.com>
2026-06-30 14:52:43 +0530

    Fixed data loader path

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from pathlib import Path
-base_dir = Path(__file__).resolve().parent.parent
-data_dir = base_dir/"data"
+
+# Project root folder
+base_dir = Path(__file__).resolve().parent
+
+# Data folder path
+data_dir = base_dir / "data"
+
+# Load datasets
 projects = pd.read_csv(data_dir / "projects.csv")
 employees = pd.read_csv(data_dir / "employees.csv")
 tasks = pd.read_csv(data_dir / "tasks.csv")
